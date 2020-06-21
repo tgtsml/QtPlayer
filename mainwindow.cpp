46d9c5cbdf1ca2519e98094fa984c4d95dbaece6
tgtsml <time_forget@outlook.com>
2020-06-21 17:11:59 +0800

    add sdl2 config

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,8 +2,10 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 
+#define SDL_MAIN_HANDLED
 extern "C"{
 #include "libavcodec/avcodec.h"
+#include "SDL2/SDL.h"
 }
 
 MainWindow::MainWindow(QWidget *parent)
@@ -12,6 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     qDebug()<<"FFmpeg config: "<<avcodec_configuration();
+    qDebug()<<"SDL init: "<<SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
 }
 
 MainWindow::~MainWindow()
