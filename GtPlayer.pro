fb9cc4cc7abcba968d0d45c776585dd43954ee3d
tgtsml <time_forget@outlook.com>
2020-06-21 16:10:29 +0800

    add ffmpeg config

--- GtPlayer.pro
+++ GtPlayer.pro
@@ -25,7 +25,6 @@ HEADERS += \
 FORMS += \
     mainwindow.ui
 
-# Default rules for deployment.
-qnx: target.path = /tmp/$${TARGET}/bin
-else: unix:!android: target.path = /opt/$${TARGET}/bin
-!isEmpty(target.path): INSTALLS += target
+INCLUDEPATH += $$PWD/include
+
+LIBS += -L$$PWD/lib -lavcodec
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+
+extern "C"{
+#include "libavcodec/avcodec.h"
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug()<<"FFmpeg config: "<<avcodec_configuration();
 }
 
 MainWindow::~MainWindow()
